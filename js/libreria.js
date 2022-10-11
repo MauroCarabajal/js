62a900ec9f126e7588b23d6d31c0f55b01919c37
Mauro Carabajal <maurotomas334@gmail.com>
2022-10-11 15:38:25 -0300

    Final

--- js/libreria.js
+++
@@ -1,8 +0,0 @@
-const btn = document.querySelector('.btn-success')
-btn.addEventListener ('click', () => { 
-    Swal.fire({
-        title: 'Compra realizada con exito',
-        icon: 'success',
-        confirmButtonText: 'Aceptar'
-    })
-    })
